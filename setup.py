2844965c5a7c8bc10c991f66bfcd9838c1dc026b
Timo Beckers <timo.beckers@klarrio.com>
2018-03-13 09:47:19 +0100

    Remove __init__ files from packages, update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup, find_packages
 
-# py_files = [
-#     "ansible/module_utils/dotdiff"
-# ]
-
-long_description=open('README.rst', 'r').read()
-version=open('VERSION', 'r').read()
+# We cannot declare __init__.py files in our package because they conflict
+# with Ansible's. This disables setuptools package detection. Specify all
+# files to be included in the package here.
+py_files = [
+    "ansible/module_utils/dotdiff"
+]
 
 setup(
     name='ansible-dotdiff',
-    version=version,
+    version=open('VERSION', 'r').read(),
     description='Nested structure diff library with dot-path notation for Ansible',
-    long_description=long_description,
+    long_description=open('README.rst', 'r').read(),
     author='Timo Beckers',
     author_email='timo.beckers@klarrio.com',
     url='https://github.com/Klarrio/ansible-dotdiff',
-    # py_modules=py_files, # required for single-file modules (like the files in module_utils)
-    packages=find_packages(),
+    py_modules=py_files,
     install_requires = [
         'ansible>=2.1.0'
     ],
